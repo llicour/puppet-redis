1224b4f3973b1ff3ac07427951bd1191e0fe9da8
lofic <louis.coilliot@gmail.com>
2013-03-06 17:01:33 +0100

    Massive reimport as an individual module

---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+class redis( $up = hiera('redis::up', true) ) {
+
+    include yum::epel
+
+    package { 'redis' :
+        ensure  => present,
+        require => File[ 'epel.repo' ],
+    }
+
+    file { 'redis.conf' :
+        ensure  => present,
+        path    => '/etc/redis.conf',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        source  => 'puppet:///modules/redis/redis.conf',
+    }
+
+    service { 'redis' :
+        ensure  => $up? { true    => running,
+                          'true'  => running,
+                          default => stopped },
+        enable  => $up? { true    => true,
+                          'true'  => true,
+                          default => false   },
+        require => Package[ 'redis' ],
+    }
+
+}
